adead477d00b761e8e52a81a1723942ee3b5558b
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-10-29 22:06:24 +0000

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -5,6 +5,17 @@ int main()
   for(i =0; i <= 100; i++)
   {
     std::cout << i << std::endl;
+    switch(i)
+    {
+      case 3:
+      std::cout << "Fizz ";
+      std::cout << i << std::endl;
+      case 5:
+      std::cout << "Buzz ";
+      std::cout << i << std::endl;
+      default:
+      std::cout << i << endl;
+    }
   }
   return 0;
 }
